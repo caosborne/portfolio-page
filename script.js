f5bb8f47fce3f309d5cf78d2b393dece4b957261
Christopher Osborne <chris.a.osborne@gmail.com>
2016-08-27 21:13:00 -0500

    Committing my portfolio site

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+
+  $('.collapse').collapse()
+
+// $('#myModal').modal('hide');
+
+$(function () {
+   $('#modal').modal('toggle');
+});
+
+// $('#jump').css({
+//     'all': 'initial',
+//     'width': '100%',
+//     'height': 'auto'
+// });
+
+});
